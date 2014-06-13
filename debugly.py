b0c591666b7a99fd547e3b35bc4b8a320cdcd17d
yiqing <yiqing-95@qq.com>
2014-06-14 00:43:30 +0800

    minor modifying

--- debugly.py
+++ debugly.py
@@ -1,7 +1,10 @@
 __author__ = 'yiqing'
 
+from functools import wraps
+
 def debug(func):
     # func is function to be wrapped
+    @wraps(func)
     def wrapper(*args,**kwargs):
         print(func.__name__)
         return func(*args,**kwargs)
