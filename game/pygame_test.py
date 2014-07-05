94a8117ce657f5d3f9ef3e396781b0985fb3f14c
yiqing <yiqing-95@qq.com>
2014-07-05 12:01:48 +0800

    动画

--- game/pygame_test.py
+++ game/pygame_test.py
@@ -7,9 +7,11 @@ class Game(object):
         clock = pygame.time.Clock()
 
         image = pygame.image.load('player.png')
+        image_x = 320
+        image_y = 240
 
         while 1:
-            # 歇息下！
+            # 歇息下！ less CPU used
             clock.tick(30)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -17,9 +19,13 @@ class Game(object):
                 if event.type == pygame.KEYDOWN and \
                     event.key == pygame.K_ESCAPE:
                     return
+
+            image_x += 10
+
             screen.fill((200,200,200))
             # blit 是block image transfer 的缩写！
             screen.blit(image,(320,240))
+            screen.blit(image,(image_x,image_y))
             # 双缓冲技术 交换display buffer跟drawing buffer的指针
             pygame.display.flip()
 
